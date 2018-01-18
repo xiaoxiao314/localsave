dde3e3937cebdbc97ec76ffaeb6c4cb36ad66a6d
15232199565@163.com <15232199565@163.com>
2018-01-18 13:05:51 +0800

    test

--- localsave.js
+++ localsave.js
@@ -1,33 +1,19 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-function localsave() {
-  this.setData = function(src,data) {
-    let value = JSON.stringify(data)
-    localStorage.setItem(src, value)
-    return
-  };
-  this.getData = function(src) {
-    let value = JSON.parse(localStorage.getItem(src))
-    return value
-  };
-  this.run = function(src,data) {
-    if(this.getData(src)){
-      return this.getData(src)
-    } else {
-      this.setData(src,data)
-      return this.getData(src)
-    }
-  };
+var localsave = function () {}
+localsave.setData = function(src,data) {
+  let value = JSON.stringify(data)
+  localStorage.setItem(src, value)
+  return
 };
-
-module.exports = localsave;
-
-
-
-
-
-
-
-
+localsave.getData = function(src) {
+  let value = JSON.parse(localStorage.getItem(src))
+  return value
+};
+localsave.run = function(src,data) {
+  if(this.getData(src)){
+    return this.getData(src)
+  } else {
+    this.setData(src,data)
+    return this.getData(src)
+  }
+};
+export {localsave}
