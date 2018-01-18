2051ee512615f2379dc14745fbb69732193b5210
15232199565@163.com <15232199565@163.com>
2018-01-18 15:58:28 +0800

    done

--- localsave.js
+++ localsave.js
@@ -1,19 +1,21 @@
 var localsave = function () {}
-localsave.setData = function(src,data) {
-  let value = JSON.stringify(data)
-  localStorage.setItem(src, value)
-  return
+
+localsave.setAndGetData = function(src) {
+    reqwest({
+        url: url,
+        method: 'get',
+        type: 'json',
+        success (resp) {
+            let value = JSON.stringify(resp)
+            localStorage.setItem(src, value)
+            return resp
+        }
+    })
 };
+
 localsave.getData = function(src) {
-  let value = JSON.parse(localStorage.getItem(src))
-  return value
+    let value = JSON.parse(localStorage.getItem(src))
+    return value
 };
-localsave.run = function(src,data) {
-  if(this.getData(src)){
-    return this.getData(src)
-  } else {
-    this.setData(src,data)
-    return this.getData(src)
-  }
-};
+
 export {localsave}
